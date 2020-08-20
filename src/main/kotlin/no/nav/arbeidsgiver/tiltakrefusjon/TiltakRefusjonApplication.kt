45d224d64caa5b2817bb21c8cba9d56d13db624f
Mats Bolstad <mbolstad@gmail.com>
2020-08-20 22:21:17 +0200

    Sjekk på at man kjører riktig opp riktig application-klasse

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/TiltakRefusjonApplication.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/TiltakRefusjonApplication.kt
@@ -2,12 +2,17 @@ package no.nav.arbeidsgiver.tiltakrefusjon
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import kotlin.system.exitProcess
 
 @SpringBootApplication
 class TiltakRefusjonApplication
 
 fun main(args: Array<String>) {
     runApplication<TiltakRefusjonApplication>(*args) {
+        if (System.getenv("NAIS_CLUSTER_NAME") == null) {
+            println("Kan ikke startes uten miljøvariabel NAIS_CLUSTER_NAME. Lokalt kan TestTiltakRefusjonApplication kjøres.")
+            exitProcess(1)
+        }
         setAdditionalProfiles(System.getenv("NAIS_CLUSTER_NAME"))
     }
 }

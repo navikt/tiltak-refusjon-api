c386fa1595dae9bd986b149d88422bbc4a27093b
millad.dagdoni@nav.no <541216bf17e99fdd00adce499023629fe503a74c>
2020-10-23 14:24:48 +0200

    lag til en test som sjekker at fnr er gyldig

--- src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/FnrTest.kt
+++ src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/FnrTest.kt
@@ -1,10 +1,11 @@
 package no.nav.arbeidsgiver.tiltakrefusjon.refusjon
 
 import no.nav.arbeidsgiver.tiltakrefusjon.exceptions.RefusjonException
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 
-internal class FnrTest {
+class FnrTest {
     @Test
     fun `gitt et tom fnr skal det kastes en exception`() {
         assertThrows(RefusjonException::class.java,{
@@ -14,22 +15,34 @@ internal class FnrTest {
 
     @Test
     fun `gitt kort fnr skal det kastes en exception`() {
-        assertThrows(RefusjonException::class.java,{
-            val fnr:Fnr = Fnr("123")
-        });
+        assertThrows(RefusjonException::class.java) {
+            Fnr("123")
+        };
     }
 
     @Test
     fun `gitt fnr med flere enn 11 tall skal det kastes en exception`() {
         assertThrows(RefusjonException::class.java,{
-            val fnr:Fnr = Fnr("1234567890123")
+            Fnr("1234567890123")
         });
     }
 
     @Test
     fun `gitt fnr med bokstaver og tall skal det kastes en exception`() {
         assertThrows(RefusjonException::class.java,{
-            val fnr:Fnr = Fnr("1123z5678901")
+            Fnr("1123z5678901")
         });
+    }
+
+    @Test
+    fun `gitt fnr andre ting enn tall skal det kastes en exception`() {
+        assertThrows(RefusjonException::class.java,{
+           Fnr("12345678900 ")
+        });
+    }
+
+    @Test fun `gitt gyldig fnr skal true returneres`(){
+        val gyldigFnrMed11Tall = "01234567890"
+        assertThat(Fnr(gyldigFnrMed11Tall).verdi).isEqualTo(gyldigFnrMed11Tall)
     }
 }

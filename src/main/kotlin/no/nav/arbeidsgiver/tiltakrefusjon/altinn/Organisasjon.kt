990a64d02cce4a126a5285a878c98219ebf1cc46
havardmj <Havard.Myrvold.Johannessen@nav.no>
2020-10-23 10:38:32 +0200

    Lagt til Json case-sensitive på mapping av data klasse
    Co-authored-by: Millad Dagdoni <millad.dagdoni@nav.no>

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/altinn/Organisasjon.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/altinn/Organisasjon.kt
@@ -1,10 +1,14 @@
 package no.nav.arbeidsgiver.tiltakrefusjon.altinn
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.UpperCamelCaseStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(UpperCamelCaseStrategy::class)
 data class Organisasjon (
         val Name: String,
-    val Type: String,
-    val OrganizationNumber: String,
-    val OrganizationForm: String,
-    val Status: String,
-    val ParentOrganizationNumber: String? = null
+        val Type: String,
+        val OrganizationNumber: String,
+        val OrganizationForm: String,
+        val Status: String,
+        val ParentOrganizationNumber: String? = null
 )

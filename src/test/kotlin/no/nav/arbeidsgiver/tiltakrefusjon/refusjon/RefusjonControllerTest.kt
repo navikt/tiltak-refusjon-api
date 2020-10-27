308fb412557edc6cd4f939c64b14a5f661df748d
millad.dagdoni@nav.no <541216bf17e99fdd00adce499023629fe503a74c>
2020-10-27 07:43:55 +0100

    lagt en enkel test som tester at RefusjonController.kt kan opprettes

--- src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonControllerTest.kt
+++ src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonControllerTest.kt
@@ -1,4 +1,21 @@
 package no.nav.arbeidsgiver.tiltakrefusjon.refusjon
 
-//TODO: Test meg
-internal class RefusjonControllerTest
+import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+import org.mockito.junit.jupiter.MockitoExtension
+@ExtendWith(MockitoExtension::class)
+class RefusjonControllerTest{
+
+    val refusjonRepository:RefusjonRepository = mockk<RefusjonRepository>()
+    val refusjonController:RefusjonController = RefusjonController(refusjonRepository)
+
+
+    @Test fun `skal kunne opprette klassen`(){
+        assertThat(refusjonController).isNotNull
+    }
+
+
+}

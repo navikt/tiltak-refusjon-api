b2251731ca06bdb7d49a17798fa48710c8ab97c9
millad.dagdoni@nav.no <541216bf17e99fdd00adce499023629fe503a74c>
2020-10-27 09:43:00 +0100

    endret Organisasjon.kt til å inneholder lowercased variablenavn

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/altinn/Organisasjon.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/altinn/Organisasjon.kt
@@ -5,10 +5,10 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming
 
 @JsonNaming(UpperCamelCaseStrategy::class)
 data class Organisasjon (
-        val Name: String,
-        val Type: String,
-        val OrganizationNumber: String,
-        val OrganizationForm: String,
-        val Status: String,
-        val ParentOrganizationNumber: String? = null
+        val name: String,
+        val type: String,
+        val organizationNumber: String,
+        val organizationForm: String,
+        val status: String,
+        val parentOrganizationNumber: String? = null
 )

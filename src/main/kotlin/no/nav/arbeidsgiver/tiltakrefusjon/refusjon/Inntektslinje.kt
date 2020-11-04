1a24f3bd4ebd79338aa6a27cafac02be1df4af54
millad.dagdoni@nav.no <541216bf17e99fdd00adce499023629fe503a74c>
2020-11-04 10:59:29 +0100

    Endret Inntektslinje.kt inntektsperiode til opptjenningsperiode for å gjenspeile inntektskomponent response og domene

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/Inntektslinje.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/Inntektslinje.kt
@@ -8,15 +8,15 @@ data class Inntektslinje(
         val inntektType: String = "LØNNSINNTEKT",
         val beløp: Double = 0.0,
         val måned: YearMonth = YearMonth.now(),
-        val inntektsperiodeFom: LocalDate = måned.atDay(1),
-        val inntektsperiodeTom: LocalDate = måned.atEndOfMonth()
+        val opptjeningsperiodeFom: LocalDate = måned.atDay(1),
+        val opptjenningsperiodeTom: LocalDate = måned.atEndOfMonth()
 ) {
 
     constructor(inntektType:String, beløp: Double?, måned: YearMonth, inntektsperiodeFom: LocalDate? = null, inntektsperiodeTom: LocalDate? = null):
             this(inntektType,beløp ?: 0.0 ,måned, inntektsperiodeFom ?: måned.atDay(1), inntektsperiodeTom ?: måned.atEndOfMonth())
 
     fun hentAntallOpptjenteDagerInnenPeriode(fraDato: LocalDate, tilDato: LocalDate): Int {
-        return inntektsperiodeFom.datesUntil(inntektsperiodeTom.plusDays(1))
+        return opptjeningsperiodeFom.datesUntil(opptjenningsperiodeTom.plusDays(1))
                 .filter(this::erHverdag)
                 .filter{ !it.isBefore(fraDato) && !it.isAfter(tilDato) }
                 .count()

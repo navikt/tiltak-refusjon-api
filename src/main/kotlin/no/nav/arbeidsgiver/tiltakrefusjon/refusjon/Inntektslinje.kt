be69e9c0bfe6fac514d3209f5806046e02ecdddf
millad.dagdoni@nav.no <541216bf17e99fdd00adce499023629fe503a74c>
2020-11-05 13:37:45 +0100

    ryddet opp Inntektslinje.kt  henting av antall opptjente dager som en funksjon

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/Inntektslinje.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/Inntektslinje.kt
@@ -3,6 +3,7 @@ package no.nav.arbeidsgiver.tiltakrefusjon.refusjon
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.YearMonth
+import kotlin.streams.toList
 
 data class Inntektslinje(
         val inntektType: String = "LØNNSINNTEKT",
@@ -17,19 +18,17 @@ data class Inntektslinje(
 
 
     fun hentAntallDagerOpptjent():Int{
-        return opptjeningsperiodeFom.datesUntil(opptjenningsperiodeTom.plusDays(1))
-                .filter(this::erHverdag)
+        return dagerOpptjentList()
                 .count()
-                .toInt()
     }
 
     fun hentAntallOpptjenteDagerInnenPeriode(fraDato: LocalDate, tilDato: LocalDate): Int {
-        return opptjeningsperiodeFom.datesUntil(opptjenningsperiodeTom.plusDays(1))
-                .filter(this::erHverdag)
+        return dagerOpptjentList()
                 .filter{ !it.isBefore(fraDato) && !it.isAfter(tilDato) }
                 .count()
-                .toInt()
     }
+
+    private fun dagerOpptjentList() = opptjeningsperiodeFom.datesUntil(opptjenningsperiodeTom.plusDays(1)).filter(this::erHverdag).toList()
 
     private fun erHverdag(dato: LocalDate): Boolean {
         return dato.dayOfWeek != DayOfWeek.SATURDAY && dato.dayOfWeek != DayOfWeek.SUNDAY

c911ba763d4952c84bfbc3252afe2f77bf3b6866
Millad Dagdoni <millad.dagdoni@nav.no>
2020-11-11 13:34:02 +0100

    Endret RefusjonsberegningRequest.kt til rettet feilmelding i exception

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonsberegningService.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonsberegningService.kt
@@ -8,7 +8,7 @@ import java.time.LocalDate
 class RefusjonsberegningService(val refusjonRepository: RefusjonRepository, val inntektskomponentConsumer: InntektskomponentConsumer) {
     fun hentGrunnlag(refusjonsberegningRequest: RefusjonsberegningRequest):Refusjonsgrunnlag{
         if(!refusjonsberegningRequest.erUtfylt()){
-            throw RefusjonException("Refusjonsberegning er ikke utfylt av bruker")
+            throw RefusjonException("Refusjonsberegning er ikke riktig utfylt")
         }
         val inntekter = inntektskomponentConsumer.hentInntekter(refusjonsberegningRequest)
         val refusjon:Refusjon =  refusjonRepository.findOneByDeltakerFnrAndBedriftnummerAndFraDatoGreaterThanEqualAndTilDatoLessThanEqual(refusjonsberegningRequest.fnr, refusjonsberegningRequest.bedriftNr, LocalDate.parse(refusjonsberegningRequest.refusjonFraDato), LocalDate.parse(refusjonsberegningRequest.refusjonTilDato))

3302b288541e5b38b980f49f9dee6f722ee82218
Millad Dagdoni <millad.dagdoni@nav.no>
2020-11-11 13:47:31 +0100

    Endret RefusjonsberegningRequest.kt til final val for hver variabel deklarering

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonsberegningRequest.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonsberegningRequest.kt
@@ -2,10 +2,10 @@ package no.nav.arbeidsgiver.tiltakrefusjon.refusjon
 
 
 data class RefusjonsberegningRequest  (
-        var fnr: String,
-        var bedriftNr: String,
-        var refusjonFraDato: String,
-        var refusjonTilDato: String
+        val fnr: String,
+        val bedriftNr: String,
+        val refusjonFraDato: String,
+        val refusjonTilDato: String
 ){
     fun erUtfylt():Boolean{
         return !(fnr.isBlank() || bedriftNr.isBlank() || refusjonFraDato.isBlank() || refusjonTilDato.isBlank())

ce7c499b2a9037006a0896a263ab79469431364f
Mats Bolstad <mbolstad@gmail.com>
2020-11-17 16:37:54 +0100

    Mulighet for å sette authorization header på swagger

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/SwaggerConfig.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/SwaggerConfig.kt
@@ -2,19 +2,29 @@ package no.nav.arbeidsgiver.tiltakrefusjon
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ParameterBuilder
 import springfox.documentation.builders.PathSelectors
+import springfox.documentation.schema.ModelRef
 import springfox.documentation.spi.DocumentationType.SWAGGER_2
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+
 
 @Configuration
 @EnableSwagger2
 class SwaggerConfig {
     @Bean
     fun api(): Docket {
+        val parameters = ParameterBuilder().name("Authorization")
+                .modelRef(ModelRef("string"))
+                .parameterType("header")
+                .description("JWT token")
+                .required(false)
+                .build()
         return Docket(SWAGGER_2)
                 .select()
                 .paths(PathSelectors.any())
                 .build()
+                .globalOperationParameters(listOf(parameters))
     }
 }

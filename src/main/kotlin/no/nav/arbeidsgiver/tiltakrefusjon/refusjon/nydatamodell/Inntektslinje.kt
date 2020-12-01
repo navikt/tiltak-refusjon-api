153e093db39453bc1113284a4a65b04c8879ec61
Mats Bolstad <mbolstad@gmail.com>
2020-12-01 23:00:14 +0100

    Kopierer logikk i inntektslinje

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/nydatamodell/Inntektslinje.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/nydatamodell/Inntektslinje.kt
@@ -1,9 +1,11 @@
 package no.nav.arbeidsgiver.tiltakrefusjon.refusjon.nydatamodell
 
 import com.github.guepardoapps.kulid.ULID
+import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.YearMonth
 import javax.persistence.*
+import kotlin.streams.toList
 
 @Entity
 @Table(name = "inntektslinje")
@@ -18,5 +20,22 @@ data class InntektslinjeEntity(
 ) {
     @ManyToOne
     @JoinColumn(name = "inntektsgrunnlag_id")
-    lateinit var inntektsgrunnlag: Inntektsgrunnlag
+    internal lateinit var inntektsgrunnlag: Inntektsgrunnlag
+
+    @Transient
+    private val dagerOpptjent = (opptjeningsperiodeFom ?: måned.atDay(1)).datesUntil((opptjeningsperiodeTom
+            ?: måned.atEndOfMonth()).plusDays(1)).filter(::erHverdag).toList()
+
+    @Transient
+    private val antallDagerOpptjent = dagerOpptjent.count()
+
+    fun antallDagerOpptjent(fraDato: LocalDate, tilDato: LocalDate) = dagerOpptjent.filter { !it.isBefore(fraDato) && !it.isAfter(tilDato) }.count()
+
+    @Transient
+    var beløpPerDag = beløp / antallDagerOpptjent
+
+    @Transient
+    val erLønnsinntekt = inntektType == "LOENNSINNTEKT" && beløp > 0.0
 }
+
+fun erHverdag(dato: LocalDate) = dato.dayOfWeek != DayOfWeek.SATURDAY && dato.dayOfWeek != DayOfWeek.SUNDAY

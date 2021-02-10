68df444e12ab836fb9a6ce625cf4f5a703b6e125
Millad Dagdoni <millad.dagdoni@nav.no>
2021-02-10 10:36:12 +0100

    fjernet labs-gcp fra TestDataLaster.kt (#7)

---
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/okonomi/FakeKontoregisterkomponentService.kt
@@ -0,0 +1,14 @@
+package no.nav.arbeidsgiver.tiltakrefusjon.okonomi
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Service
+
+@Service
+@ConditionalOnProperty("tiltak-refusjon.kontoregister.fake")
+class FakeKontoregisterkomponentService : KontoregisterkomponentService {
+    override fun hentBankkontonummer(
+        bedriftNr: String
+    ): String {
+       return "10000008162";
+    }
+}
---
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/okonomi/KontoregisterkomponentProperties.kt
@@ -0,0 +1,12 @@
+package no.nav.arbeidsgiver.tiltakrefusjon.okonomi
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+import java.net.URI
+
+@Component
+@ConfigurationProperties(prefix = "tiltak-refusjon.kontoregister")
+data class KontoregisterkomponentProperties(
+        var uri: URI = URI(""),
+        var consumerId: String = ""
+)

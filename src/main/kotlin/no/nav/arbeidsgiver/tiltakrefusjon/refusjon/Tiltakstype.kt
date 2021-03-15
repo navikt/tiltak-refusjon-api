8684a4020c512d70abea212f6ebeea7725e62bd0
Mats Bolstad <mbolstad@gmail.com>
2021-03-15 11:18:46 +0100

    Ny tiltakstype for sommerjobb

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/Tiltakstype.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/Tiltakstype.kt
@@ -1,5 +1,5 @@
 package no.nav.arbeidsgiver.tiltakrefusjon.refusjon
 
 enum class Tiltakstype {
-    MIDLERTIDIG_LONNSTILSKUDD, VARIG_LONNSTILSKUDD
+    MIDLERTIDIG_LONNSTILSKUDD, VARIG_LONNSTILSKUDD, SOMMERJOBB
 }

55b5774734044e377d6a9e397a9898993b638906
Millad Dagdoni <millad.dagdoni@nav.no>
2021-05-03 11:05:05 +0200

    Fikset byggfeil ved å bruke gyldig testdata
    Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>
    Co-authored-by: Magnus Rom <magnus.skalberg.rom@nav.no>

--- src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonGodkjentKafkaLytter.kt
+++ src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonGodkjentKafkaLytter.kt
@@ -2,10 +2,10 @@ package no.nav.arbeidsgiver.tiltakrefusjon.refusjon
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import no.nav.arbeidsgiver.tiltakrefusjon.Topics
+import no.nav.arbeidsgiver.tiltakrefusjon.enRefusjon
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
-import java.time.LocalDate
 
 @ConditionalOnProperty("tiltak-refusjon.kafka.enabled")
 @Component
@@ -14,10 +14,6 @@ class RefusjonGodkjentKafkaLytter(val service: RefusjonService, val objectMapper
     @KafkaListener(topics = [Topics.REFUSJON_GODKJENT])
     fun tilskuddsperiodeGodkjent(refusjonGodkjentMelding:  String) {
         val godkjentMelding = objectMapper.readValue(refusjonGodkjentMelding, RefusjonGodkjentMelding::class.java)
-        service.godkjennForArbeidsgiver(Refusjon(tilskuddsgrunnlag = Tilskuddsgrunnlag("avtaleid","tspid",
-            "Donald","duck","212","z123","Duck co","123",
-             LocalDate.now(), LocalDate.now().plusMonths(2),0.12,0.2,0.12,
-            Tiltakstype.MIDLERTIDIG_LONNSTILSKUDD,123,401,1,1)
-            ,"102","123"))
+       service.godkjennForArbeidsgiver(enRefusjon())
     }
 }
--- src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonKafkaProducerTest.kt
+++ src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonKafkaProducerTest.kt
@@ -1,12 +1,13 @@
 package no.nav.arbeidsgiver.tiltakrefusjon.refusjon
 
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.verify
 import no.nav.arbeidsgiver.tiltakrefusjon.Topics
 import org.awaitility.Awaitility
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.test.context.EmbeddedKafka
 import org.springframework.test.annotation.DirtiesContext
@@ -24,7 +25,7 @@ class RefusjonKafkaProducerTest {
     @Autowired
     lateinit var kafkaTemplate: KafkaTemplate<String, RefusjonGodkjentMelding>
 
-    @MockBean
+    @MockkBean
     lateinit var refusjonService: RefusjonService
 
     @Autowired
@@ -37,16 +38,15 @@ class RefusjonKafkaProducerTest {
             "Donald","duck","212","z123","Duck co","123",
         1000, LocalDate.now(), LocalDate.now().plusMonths(2),0.12,0.2,0.12,40,123,1)
 
-       val refusjon= Refusjon(tilskuddsgrunnlag = Tilskuddsgrunnlag("avtaleid","tspid",
-            "Donald","duck","212","z123","Duck co","123",
-            LocalDate.now(), LocalDate.now().plusMonths(2),0.12,0.2,0.12,
-            Tiltakstype.MIDLERTIDIG_LONNSTILSKUDD,123,401,1,1)
-            ,"102","123")
+        every {
+            refusjonService.godkjennForArbeidsgiver(any())
+        } returns Unit
+
         // NÅR
         kafkaTemplate.send(Topics.REFUSJON_GODKJENT, refusjonGodkjent.tilskuddsperiodeId, refusjonGodkjent)
 
         // SÅ
-        Awaitility.await().atMost(30, TimeUnit.SECONDS).untilAsserted { Mockito.verify(refusjonService).godkjennForArbeidsgiver(refusjon) }
+        Awaitility.await().atMost(30, TimeUnit.SECONDS).untilAsserted { verify{refusjonService.godkjennForArbeidsgiver(any())} }
     }
 
 }

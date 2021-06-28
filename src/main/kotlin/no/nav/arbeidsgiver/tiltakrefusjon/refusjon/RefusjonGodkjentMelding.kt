0b071eba0e4f9cf2b291acfec673bff115f6aca7
Millad Dagdoni <millad.dagdoni@nav.no>
2021-06-28 11:09:08 +0200

    lagt tilbake RefusjonGodkjentMelding.kt
    Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonGodkjentMelding.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonGodkjentMelding.kt
@@ -1,15 +1,49 @@
 package no.nav.arbeidsgiver.tiltakrefusjon.refusjon
 
+import java.time.LocalDate
 
 data class RefusjonGodkjentMelding(
-        val beløp: Int
-      )
+        val avtaleId: String,
+        val tilskuddsperiodeId: String,
+        val tiltakstype: Tiltakstype,
+        val deltakerFornavn: String,
+        val deltakerEtternavn: String,
+        val deltakerFnr: String,
+        val veilederNavIdent: String,
+        val bedriftNavn: String,
+        val bedriftNr: String,
+        val beløp: Int,
+        val refusjonId: String,
+        val tilskuddFom: LocalDate,
+        val tilskuddTom: LocalDate,
+        val feriepengerSats: Double,
+        val otpSats: Double,
+        val arbeidsgiveravgiftSats: Double,
+        val tilskuddsprosent: Int,
+        val avtaleNr: Int,
+        val løpenummer: Int,
+        val bedriftKontonummer: String)
         {
         companion object{
                 @JvmStatic
                 fun create(refusjon: Refusjon): RefusjonGodkjentMelding {
-                        return RefusjonGodkjentMelding(
-                                refusjon.tilskuddsgrunnlag.tilskuddsbeløp
+                        return RefusjonGodkjentMelding(refusjon.tilskuddsgrunnlag.avtaleId, refusjon.tilskuddsgrunnlag.tilskuddsperiodeId,
+                                refusjon.tilskuddsgrunnlag.tiltakstype,refusjon.tilskuddsgrunnlag.deltakerFornavn,
+                                refusjon.tilskuddsgrunnlag.deltakerEtternavn,refusjon.deltakerFnr,
+                                refusjon.tilskuddsgrunnlag.veilederNavIdent,
+                                refusjon.tilskuddsgrunnlag.bedriftNavn,
+                                refusjon.tilskuddsgrunnlag.bedriftNr,
+                                refusjon.tilskuddsgrunnlag.tilskuddsbeløp,
+                                refusjon.id,
+                                refusjon.tilskuddsgrunnlag.tilskuddFom,
+                                refusjon.tilskuddsgrunnlag.tilskuddTom,
+                                refusjon.tilskuddsgrunnlag.feriepengerSats,
+                                refusjon.tilskuddsgrunnlag.otpSats,
+                                refusjon.tilskuddsgrunnlag.arbeidsgiveravgiftSats,
+                                refusjon.tilskuddsgrunnlag.lønnstilskuddsprosent,
+                                refusjon.tilskuddsgrunnlag.avtaleNr,
+                                refusjon.tilskuddsgrunnlag.løpenummer,
+                                refusjon.bedriftKontonummer!!
                         )
                 }
         }

efb6a75a0f02a709a1b571c6dfa57c29efad7c78
Sindre Dahl Løken <sindre.loken@nav.no>
2021-07-13 16:48:03 +0200

    lagt til component på kafka-producer og logging Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonVarselProducer.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonVarselProducer.kt
@@ -4,17 +4,22 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import no.nav.arbeidsgiver.tiltakrefusjon.Topics
 import no.nav.arbeidsgiver.tiltakrefusjon.refusjon.events.RefusjonKlar
 import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.support.SendResult
+import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionalEventListener
 import org.springframework.util.concurrent.ListenableFutureCallback
 
+@ConditionalOnProperty("tiltak-refusjon.kafka.enabled")
+@Component
 class RefusjonVarselProducer(val kafkaTemplate: KafkaTemplate<String, RefusjonVarselMelding>, val objectMapper: ObjectMapper) {
 
     val log = LoggerFactory.getLogger(RefusjonVarselProducer::class.java)
 
     @TransactionalEventListener
     fun refusjonKlar(event: RefusjonKlar){
+        log.info("refusjon-klar melding prosesseres for sending på topic: ${Topics.TILTAK_VARSEL} med key: ${event.refusjon.id}")
         val melding = RefusjonVarselMelding(event.refusjon.tilskuddsgrunnlag.avtaleId, VarselType.KLAR)
         kafkaTemplate.send(Topics.TILTAK_VARSEL, "${event.refusjon.id}-KLAR", melding)
             .addCallback(object : ListenableFutureCallback<SendResult<String?, RefusjonVarselMelding?>?> {

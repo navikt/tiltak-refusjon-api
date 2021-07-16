4807d3fc50c11d38f68bf864c4bf76125d878462
Mats Bolstad <mbolstad@gmail.com>
2021-07-16 09:46:17 +0200

    Ikke repetisjon i counternavn Co-authored-by: Sindre Løken <sindre.loken@nav.no>

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/autorisering/InnloggetArbeidsgiverController.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/autorisering/InnloggetArbeidsgiverController.kt
@@ -1,8 +1,8 @@
 package no.nav.arbeidsgiver.tiltakrefusjon.autorisering
 
+import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.MeterRegistry
 import no.nav.security.token.support.core.api.Protected
-import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,14 +12,19 @@ const val REQUEST_MAPPING_INNLOGGET_ARBEIDSGIVER = "/api/arbeidsgiver/innlogget-
 @RestController
 @RequestMapping(REQUEST_MAPPING_INNLOGGET_ARBEIDSGIVER)
 @Protected
-class InnloggetArbeidsgiverController(val innloggetBrukerService: InnloggetBrukerService, val meterRegistry: MeterRegistry) {
+class InnloggetArbeidsgiverController(
+    val innloggetBrukerService: InnloggetBrukerService,
+    val meterRegistry: MeterRegistry,
+) {
+    val counter: Counter = meterRegistry.counter("tiltak-refusjon.hent-innlogget-arbeidsgiver")
+
     init {
-        meterRegistry.counter("tiltak-refusjon.hent-innlogget-arbeidsgiver").increment(0.0)
+        counter.increment(0.0)
     }
 
     @GetMapping
     fun hentInnloggetArbeidsgiver(): InnloggetArbeidsgiver {
-        meterRegistry.counter("tiltak-refusjon.hent-innlogget-arbeidsgiver").increment()
+        counter.increment()
         return innloggetBrukerService.hentInnloggetArbeidsgiver()
     }
 }

0e93b3f75f94fe70272b379cc171b408bf9ee28d
Millad Dagdoni <millad.dagdoni@nav.no>
2021-08-17 13:35:29 +0200

    getMapping isteden for PostMapping for hjem endepunkt

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/RetryController.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/RetryController.kt
@@ -5,6 +5,7 @@ import no.nav.arbeidsgiver.tiltakrefusjon.refusjon.RefusjonService
 import no.nav.arbeidsgiver.tiltakrefusjon.tilskuddsperiode.TilskuddsperiodeGodkjentMelding
 import no.nav.security.token.support.core.api.Unprotected
 import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -14,7 +15,7 @@ class RetryController(val service: RefusjonService, val objectMapper: ObjectMapp
     var logger = LoggerFactory.getLogger(javaClass)
 
     @Unprotected
-    @PostMapping
+    @GetMapping
     fun hjem():String{
         return "Hei fra Refusjon-api controller"
     }

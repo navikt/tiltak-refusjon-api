baa8b4a4b5ae0816520a8085be605072cf5e47cf
Mats Bolstad <mbolstad@gmail.com>
2021-08-19 15:31:45 +0200

    Skjuler respons fra inntekt i respons

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/Inntektsgrunnlag.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/Inntektsgrunnlag.kt
@@ -1,5 +1,6 @@
 package no.nav.arbeidsgiver.tiltakrefusjon.refusjon
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.github.guepardoapps.kulid.ULID
 import no.nav.arbeidsgiver.tiltakrefusjon.utils.Now
 import java.time.LocalDateTime
@@ -9,6 +10,7 @@ import javax.persistence.*
 data class Inntektsgrunnlag(
         @OneToMany(mappedBy = "inntektsgrunnlag", cascade = [CascadeType.ALL], orphanRemoval = true, fetch = FetchType.EAGER)
         val inntekter: List<Inntektslinje>,
+        @JsonIgnore
         val respons: String
 ) {
     @Id

953e0f012eb46f232f1009d7f50e8de947bc2eb5
Mats Bolstad <mbolstad@gmail.com>
2021-09-03 16:32:58 +0200

    Konfig for labs

---
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/LabsSecurityClientConfiguration.kt
@@ -0,0 +1,22 @@
+package no.nav.arbeidsgiver.tiltakrefusjon
+
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+@Configuration
+@Profile("labs-gcp")
+class LabsSecurityClientConfiguration(val restTemplateBuilder: RestTemplateBuilder) {
+    @Bean
+    fun påVegneAvSaksbehandlerGraphRestTemplate() = restTemplateBuilder.build()
+
+    @Bean
+    fun påVegneAvSaksbehandlerProxyRestTemplate() = restTemplateBuilder.build()
+
+    @Bean
+    fun påVegneAvArbeidsgiverAltinnRestTemplate() = restTemplateBuilder.build()
+
+    @Bean
+    fun anonymProxyRestTemplate() = restTemplateBuilder.build()
+}

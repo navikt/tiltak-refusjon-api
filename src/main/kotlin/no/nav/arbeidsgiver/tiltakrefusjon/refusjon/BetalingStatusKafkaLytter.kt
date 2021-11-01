9a3efc45b3e172830b5f701029b603597303d265
Millad Dagdoni <millad.dagdoni@nav.no>
2021-11-01 14:43:44 +0100

    prodset betaling status

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/BetalingStatusKafkaLytter.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/BetalingStatusKafkaLytter.kt
@@ -5,14 +5,12 @@ import no.nav.arbeidsgiver.tiltakrefusjon.Topics.REFUSJON_ENDRET_BETALINGSSTATUS
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.context.annotation.Profile
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
 @ConditionalOnProperty("tiltak-refusjon.kafka.enabled")
-@Profile("local","dev-gcp")
 class BetalingStatusKafkaLytter(private val refusjonRepository: RefusjonRepository, private val objectMapper: ObjectMapper) {
 
     val log: Logger = LoggerFactory.getLogger(javaClass)

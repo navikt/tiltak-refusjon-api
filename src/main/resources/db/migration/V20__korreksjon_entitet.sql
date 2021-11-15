357aa2581b5691037d49448865bf7cbf6dc6bc88
Mats Bolstad <mbolstad@gmail.com>
2021-11-15 12:11:56 +0100

    Ny datamodell med korreksjoner som egen entitet og ikke som en spesiell refusjon. Oppretter nye tabeller i forkant av å pushe kode.
    Co-authored-by: Sindre Løken <sindre.loken@nav.no>

---
+++ src/main/resources/db/migration/V20__korreksjon_entitet.sql
@@ -0,0 +1,42 @@
+create table refusjonsgrunnlag
+(
+    id                                      varchar primary key,
+    tilskuddsgrunnlag_id                    varchar references tilskuddsgrunnlag (id),
+    inntektsgrunnlag_id                     varchar references inntektsgrunnlag (id),
+    beregning_id                            varchar references beregning (id),
+    bedrift_kontonummer                     varchar,
+    bedrift_kontonummer_innhentet_tidspunkt timestamp without time zone,
+    inntekter_kun_fra_tiltaket              boolean,
+    endret_brutto_lønn                      numeric,
+    tidligere_utbetalt                      numeric
+);
+
+alter table refusjon
+    add column refusjonsgrunnlag_id varchar references refusjonsgrunnlag (id);
+
+
+create table korreksjon
+(
+    id                     varchar primary key,
+    korrigerer_refusjon_id varchar references refusjon (id),
+    refusjonsgrunnlag_id   varchar references refusjonsgrunnlag (id),
+    korreksjonsnummer      integer,
+    deltaker_fnr           varchar(11),
+    bedrift_nr             varchar(9),
+    kostnadssted           varchar,
+    godkjent_tidspunkt     timestamp without time zone,
+    godkjent_av_nav_ident  varchar,
+    besluttet_av_nav_ident varchar,
+    besluttet_tidspunkt    timestamp without time zone,
+    status                 varchar
+);
+
+create table korreksjon_korreksjonsgrunner
+(
+    korreksjon_id      varchar references korreksjon (id),
+    korreksjonsgrunner varchar,
+    primary key (korreksjon_id, korreksjonsgrunner)
+);
+
+alter table refusjon
+    add column korreksjon_id varchar references korreksjon (id);

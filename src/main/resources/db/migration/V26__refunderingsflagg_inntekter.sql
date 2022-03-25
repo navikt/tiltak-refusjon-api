fc400047a1475f93100557b42c0666f26b2c2db0
Sindre Dahl Løken <sindre.loken@nav.no>
2022-03-25 16:49:39 +0100

    migrering for å sette alle inntektslinjer som tilhører refusjoner som allerde er godkjent til opptjent i periode. Her har man lagt all innhentet inntekt til grunn som bruttolønn, uten mulighet for å velge ja eller nei.

--- src/main/resources/db/migration/V26__refunderingsflagg_inntekter.sql
+++ src/main/resources/db/migration/V26__refunderingsflagg_inntekter.sql
@@ -1,1 +1,6 @@
 alter table inntektslinje add column er_opptjent_i_periode boolean default null;
+update inntektslinje set er_opptjent_i_periode = true where id in (
+        select i.id from inntektslinje i
+        inner join refusjonsgrunnlag rg on rg.inntektsgrunnlag_id = i.inntektsgrunnlag_id
+        inner join refusjon r on r.refusjonsgrunnlag_id = rg.id
+        where r.godkjent_av_arbeidsgiver is not null);

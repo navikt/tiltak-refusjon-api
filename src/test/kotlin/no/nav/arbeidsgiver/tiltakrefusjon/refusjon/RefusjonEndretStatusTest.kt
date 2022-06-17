c5b4a79dcf599fde0170833a6a438399b81a9e1c
Mattias Lundmark <lundmark.mattias@gmail.com>
2022-06-17 10:41:27 +0200

    Slettet test for admincontroller i for refusjon endret status

--- src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonEndretStatusTest.kt
+++ src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonEndretStatusTest.kt
@@ -68,23 +68,4 @@ class RefusjonEndretStatusTest(
         Assertions.assertThat(json.getString("refusjonId")).isNotNull
         Now.resetClock()
     }
-
-    @Test
-    fun `Rest-endepunkt i adminkontroller skal lage kafkameldinger med statusendringer`() {
-        // Nå klar for innsending
-        val refusjon = enRefusjon(
-            etTilskuddsgrunnlag().copy(
-                tilskuddFom = Now.localDate().minusMonths(1),
-                tilskuddTom = Now.localDate().minusDays(1)
-            )
-        )
-        refusjonRepository.save(refusjon)
-        adminController.sendStatuserTilKafkaTopic()
-        val record = KafkaTestUtils.getSingleRecord(consumer, Topics.REFUSJON_ENDRET_STATUS)
-        val json = JSONObject(record.value())
-        Assertions.assertThat(json.getString("status")).isEqualTo("KLAR_FOR_INNSENDING")
-        Assertions.assertThat(json.getString("bedriftNr")).isEqualTo("999999999")
-        Assertions.assertThat(json.getString("avtaleId")).isNotNull
-        Assertions.assertThat(json.getString("refusjonId")).isNotNull
-    }
 }

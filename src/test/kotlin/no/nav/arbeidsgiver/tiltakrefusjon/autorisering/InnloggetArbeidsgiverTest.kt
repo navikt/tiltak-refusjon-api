61964e74ee90721a2d161eeed21c99ce53c441e4
Millad Dagdoni <milladd@gmail.com>
2022-11-24 14:16:54 +0100

    lagt til en integrasjonstest som tester InnloggingArbeidsgiver finnRefusjon. Den skal bare returnere riktig refusjon basert på ID. Neste oppgave er å teste at den returnerer forrige minus beløp i det du henter

--- src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/autorisering/InnloggetArbeidsgiverTest.kt
+++ src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/autorisering/InnloggetArbeidsgiverTest.kt
@@ -1,12 +1,111 @@
 package no.nav.arbeidsgiver.tiltakrefusjon.autorisering
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.ninjasquad.springmockk.MockkBean
+import com.ninjasquad.springmockk.SpykBean
+import io.mockk.every
+import no.nav.arbeidsgiver.tiltakrefusjon.altinn.AltinnTilgangsstyringService
+import no.nav.arbeidsgiver.tiltakrefusjon.altinn.Organisasjon
+import no.nav.arbeidsgiver.tiltakrefusjon.inntekt.InntektskomponentService
+import no.nav.arbeidsgiver.tiltakrefusjon.organisasjon.EregClient
+import no.nav.arbeidsgiver.tiltakrefusjon.refusjon.KorreksjonRepository
+import no.nav.arbeidsgiver.tiltakrefusjon.refusjon.RefusjonRepository
+import no.nav.arbeidsgiver.tiltakrefusjon.refusjon.RefusjonService
+import no.nav.arbeidsgiver.tiltakrefusjon.refusjon.Tiltakstype
+import no.nav.arbeidsgiver.tiltakrefusjon.tilskuddsperiode.TilskuddsperiodeGodkjentMelding
+import no.nav.arbeidsgiver.tiltakrefusjon.utils.Now
+import no.nav.arbeidsgiver.tiltakrefusjon.varsling.VarslingRepository
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
+import org.springframework.test.context.ActiveProfiles
+import java.time.LocalDateTime
 
-internal class InnloggetArbeidsgiverTest {
-
+@SpringBootTest(properties = ["NAIS_APP_IMAGE=test"])
+@ActiveProfiles("local")
+@AutoConfigureWireMock(port = 8090)
+internal class InnloggetArbeidsgiverTest(
+    @Autowired
+    val refusjonService: RefusjonService,
+    @Autowired
+    val refusjonRepository: RefusjonRepository,
+    @Autowired
+    val varslingRepository: VarslingRepository
+) {
+    @SpykBean
+    lateinit var inntektskomponentService: InntektskomponentService
+    @MockkBean lateinit var altinnTilgangsstyringService: AltinnTilgangsstyringService
+    @MockkBean lateinit var korreksjonRepository: KorreksjonRepository
+    @MockkBean lateinit var eregClient: EregClient
+    @BeforeEach
+    fun setup() {
+        varslingRepository.deleteAll()
+        refusjonRepository.deleteAll()
+    }
     @Test
     fun finnRefusjon() {
+        val deltakerFnr = "00000000000"
+        val tilskuddMelding = TilskuddsperiodeGodkjentMelding(
+            avtaleId = "1",
+            tilskuddsbeløp = 1000,
+            tiltakstype = Tiltakstype.MIDLERTIDIG_LONNSTILSKUDD,
+            deltakerEtternavn = "Mus",
+            deltakerFornavn = "Mikke",
+            arbeidsgiveravgiftSats = 0.101,
+            avtaleInnholdId = "1",
+            bedriftNavn = "Bedriften AS",
+            bedriftNr = "999999999",
+            deltakerFnr = deltakerFnr,
+            feriepengerSats = 0.141,
+            otpSats = 0.02,
+            tilskuddFom =  Now.localDate().minusWeeks(4),
+            tilskuddTom = Now.localDate().minusDays(1),
+            tilskuddsperiodeId = "1",
+            veilederNavIdent = "X123456",
+            lønnstilskuddsprosent = 60,
+            avtaleNr = 3456,
+            løpenummer = 1,
+            enhet = "1000",
+            godkjentTidspunkt = LocalDateTime.now()
+        )
+        val tilskuddMelding2LittEldreMedLøpenummer2 = TilskuddsperiodeGodkjentMelding(
+            avtaleId = "1",
+            tilskuddsbeløp = 1000,
+            tiltakstype = Tiltakstype.MIDLERTIDIG_LONNSTILSKUDD,
+            deltakerEtternavn = "Mus",
+            deltakerFornavn = "Mikke",
+            arbeidsgiveravgiftSats = 0.101,
+            avtaleInnholdId = "2",
+            bedriftNavn = "Bedriften AS",
+            bedriftNr = "999999999",
+            deltakerFnr = deltakerFnr,
+            feriepengerSats = 0.141,
+            otpSats = 0.02,
+            tilskuddFom = Now.localDate().minusWeeks(3),
+            tilskuddTom = Now.localDate().minusDays(1),
+            tilskuddsperiodeId = "2",
+            veilederNavIdent = "X123456",
+            lønnstilskuddsprosent = 60,
+            avtaleNr = 3456,
+            løpenummer = 2,
+            enhet = "1000",
+            godkjentTidspunkt = LocalDateTime.now()
+        )
+
+        val refusjon1 = refusjonService.opprettRefusjon(tilskuddMelding)!!
+        val refusjon2 = refusjonService.opprettRefusjon(tilskuddMelding2LittEldreMedLøpenummer2)!!
+
+
+        every { altinnTilgangsstyringService.hentTilganger(any()) } returns setOf<Organisasjon>(Organisasjon("Bedrift AS", "Bedrift type", "999999999","Org form","Status"))
+        val innloggetArbeidsgiver = InnloggetArbeidsgiver("12345678901",altinnTilgangsstyringService,refusjonRepository,korreksjonRepository,refusjonService,eregClient)
+        val refusjonFunnet = innloggetArbeidsgiver.finnRefusjon(refusjon1.id)
+
+        assertThat(refusjonFunnet).isEqualTo(refusjon1)
     }
+
 }

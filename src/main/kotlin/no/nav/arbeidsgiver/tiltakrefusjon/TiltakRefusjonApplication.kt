0f92791ff5ae27fa91d0b861abead33c4bcc8da3
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2023-04-12 16:25:46 +0200

    Migrere fra labs til gcp
    Ettersom labs-miljøet nå er nede så flytter vi
    denne appen over til dev-gcp.
    
    Det blir da to apper i gcp-miljøet, så vi gjør
    nødvendige endringer for å skille på disse.

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/TiltakRefusjonApplication.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/TiltakRefusjonApplication.kt
@@ -9,10 +9,10 @@ class TiltakRefusjonApplication
 
 fun main(args: Array<String>) {
     runApplication<TiltakRefusjonApplication>(*args) {
-        if (System.getenv("NAIS_CLUSTER_NAME") == null) {
-            println("Kan ikke startes uten miljøvariabel NAIS_CLUSTER_NAME. Lokalt kan LokalTiltakRefusjonApplication kjøres.")
+        if (System.getenv("ENV") == null) {
+            println("Kan ikke startes uten miljøvariabel ENV. Lokalt kan LokalTiltakRefusjonApplication kjøres.")
             exitProcess(1)
         }
-        setAdditionalProfiles(System.getenv("NAIS_CLUSTER_NAME"))
+        setAdditionalProfiles(System.getenv("ENV"))
     }
 }

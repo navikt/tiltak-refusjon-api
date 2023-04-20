aab115e91b44604dc90cba58a05f42b2ffcba2bc
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2023-04-20 10:27:15 +0200

    Oppgradere spring boot til 2.7.10
    Fjerner også bruk av lombok i kodebasen.
    Og litt ubrukt kode.

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/dokgen/DokgenProperties.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/dokgen/DokgenProperties.kt
@@ -1,6 +1,5 @@
 package no.nav.arbeidsgiver.tiltakrefusjon.dokgen
 
-import lombok.Data
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
 import java.net.URI
--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/leader/LeaderPodCheck.java
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/leader/LeaderPodCheck.java
@@ -2,8 +2,9 @@ package no.nav.arbeidsgiver.tiltakrefusjon.leader;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
@@ -16,8 +17,9 @@ import java.util.List;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 @Component
-@Slf4j
 public class LeaderPodCheck {
+
+    private static final Logger log = LoggerFactory.getLogger(LeaderPodCheck.class);
 
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
@@ -54,11 +56,10 @@ public class LeaderPodCheck {
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(List.of(MediaType.ALL));
         var entity = new HttpEntity<>(headers);
-        ResponseEntity responseEntity = restTemplate.exchange(electorPath, HttpMethod.GET, entity, String.class);
+        var responseEntity = restTemplate.exchange(electorPath, HttpMethod.GET, entity, String.class);
         return objectMapper.readValue((String) responseEntity.getBody(), Elector.class);
     }
 
-    @Data
     private static class Elector {
         String name;
     }

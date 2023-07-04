36a790c1d651f5b95b1e45377f27653dbb84f3fb
Sindre Dahl Løken <sindre.loken@nav.no>
2023-07-04 14:17:55 +0200

    legger til hvilken refusjonId som gjør opp et minusbeløp (#73)
    * legger til hvilken refusjonId som gjør opp et minusbeløp
    
    * en test som tester at minusbeløpet blir gjort opp av refusjonen
    
    * renamer migration script for å matche flyway migration number

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/Minusbelop.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/Minusbelop.kt
@@ -9,7 +9,8 @@ class Minusbelop(
     val avtaleNr: Int,
     var beløp: Int? = null,
     var løpenummer: Int? = null,
-    var gjortOpp: Boolean = false
+    var gjortOpp: Boolean = false,
+    var gjortOppAvRefusjonId: String? = null
 ) {
     @Id
     val id: String = ULID.random()

13551a381e330785e234b91b10261d2816f60e35
Sindre Dahl Løken <sindre.loken@nav.no>
2023-08-11 14:29:31 +0200

    enkel try-catch feilhåndtering i katt mot norg

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/norg/NorgServiceImp.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/norg/NorgServiceImp.kt
@@ -1,6 +1,8 @@
 package no.nav.arbeidsgiver.tiltakrefusjon.norg
 
 import no.nav.arbeidsgiver.tiltakrefusjon.utils.ConditionalOnPropertyNotEmpty
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.beans.factory.annotation.Qualifier
@@ -9,10 +11,16 @@ import org.springframework.context.annotation.Profile
 @Service
 @Profile("dev-gcp", "prod-gcp")
 class NorgServiceImp(@Qualifier("anonymProxyRestTemplate") val restTemplate: RestTemplate, val properties: NorgProperties) : NorgService {
+    val log: Logger = LoggerFactory.getLogger(javaClass)
 
     override fun hentEnhetNavn(enhet: String): String? {
         val uri = properties.uri + "/enhet/" + enhet
-        val norgResponse = restTemplate.getForEntity(uri, NorgEnhetResponse::class.java)
-        return norgResponse.body?.navn ?: null
+        try {
+            val norgResponse = restTemplate.getForEntity(uri, NorgEnhetResponse::class.java)
+            return norgResponse.body?.navn ?: null
+        } catch (e: Exception) {
+            log.error("Kall mot Norg feilet", e)
+            return null
+        }
     }
 }

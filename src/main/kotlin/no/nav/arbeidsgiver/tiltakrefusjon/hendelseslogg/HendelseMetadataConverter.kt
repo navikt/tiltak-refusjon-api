1a5a8e56c38b8088823ffcc77eb495ac2b2e663b
Mickel Hoang m1 <mickel.hoang@bekk.no>
2023-10-17 14:33:11 +0200

    HendelseMetadata som JSON
    Co-authored-by: Odd A <3494925+Oddsor@users.noreply.github.com>

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/hendelseslogg/HendelseMetadataConverter.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/hendelseslogg/HendelseMetadataConverter.kt
@@ -5,12 +5,12 @@ import jakarta.persistence.AttributeConverter
 import jakarta.persistence.Converter
 
 val mapper = jacksonObjectMapper()
-//@Converter(autoApply = true)
-class JpaConverterMetadata: AttributeConverter<HendelseMetadata, String> {
+@Converter(autoApply = true)
+class HendelseMetadataConverter: AttributeConverter<HendelseMetadata, String> {
 
     override fun convertToDatabaseColumn(attribute: HendelseMetadata?): String? =
-        if (attribute == null) null else mapper.writeValueAsString(attribute)
-
+        if (attribute == null) null
+        else mapper.writeValueAsString(attribute)
 
     override fun convertToEntityAttribute(dbData: String?): HendelseMetadata? =
         if (dbData == null) null
--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/hendelseslogg/Hendelseslogg.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/hendelseslogg/Hendelseslogg.kt
@@ -5,16 +5,19 @@ import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import no.nav.arbeidsgiver.tiltakrefusjon.utils.Now
 import no.nav.arbeidsgiver.tiltakrefusjon.utils.ulid
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
 import java.time.LocalDateTime
 
 @Entity
-class Hendelseslogg(
+data class Hendelseslogg(
     val appImageId: String,
     val refusjonId: String,
     val korreksjonId: String?,
     val utførtAv: String,
     val event: String,
-    @Convert(converter = JpaConverterMetadata::class)
+    @Convert(converter = HendelseMetadataConverter::class)
+    @JdbcTypeCode(SqlTypes.JSON)
     val metadata: HendelseMetadata? = null,
 ) {
     @Id

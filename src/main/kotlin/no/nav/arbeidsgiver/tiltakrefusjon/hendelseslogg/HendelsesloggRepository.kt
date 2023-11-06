258e9aaa970db4f370e0ea21bd0caf5cd10eb806
Mickel <Majjki@users.noreply.github.com>
2023-11-06 12:20:04 +0100

    Hendelselogg henter alle hendelser på refusjonsId (#102)
    * Hendelselogg henter alle hendelser på refusjonsId

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/hendelseslogg/HendelsesloggRepository.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/hendelseslogg/HendelsesloggRepository.kt
@@ -2,4 +2,6 @@ package no.nav.arbeidsgiver.tiltakrefusjon.hendelseslogg
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface HendelsesloggRepository : JpaRepository<Hendelseslogg, String>
+interface HendelsesloggRepository : JpaRepository<Hendelseslogg, String>{
+    fun findAllByRefusjonId(refusjonId: String): List<Hendelseslogg>
+}

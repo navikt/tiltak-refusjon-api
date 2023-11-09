e99b3a6d7d230a9bb63ed30fd2ae71df11bf3455
Mattias Lundmark <lundmark.mattias@gmail.com>
2023-11-09 10:55:42 +0100

    Fikset exception hvor pdf skulle genereres på refusjon med nullbeløp

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/dokgen/RefusjonTilPDFMapper.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/dokgen/RefusjonTilPDFMapper.kt
@@ -2,17 +2,13 @@ package no.nav.arbeidsgiver.tiltakrefusjon.dokgen
 
 import no.nav.arbeidsgiver.tiltakrefusjon.pdf.RefusjonTilPDF
 import no.nav.arbeidsgiver.tiltakrefusjon.refusjon.Refusjon
+import no.nav.arbeidsgiver.tiltakrefusjon.refusjon.RefusjonStatus
 import java.time.LocalDate
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 object RefusjonTilPDFMapper {
     fun tilPDFdata(refusjon : Refusjon) : RefusjonTilPDF {
-
-        if(refusjon.refusjonsgrunnlag.beregning == null){
-            throw RuntimeException("Beregning er null")
-        }
-
         val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.YYYY")
         var godkjentArbeidsgiverDato: String =  ""
         var utbetaltDato:String = ""
@@ -26,48 +22,87 @@ object RefusjonTilPDFMapper {
             bedriftKid = refusjon.refusjonsgrunnlag.bedriftKid!!
         }
 
-
-
         val tilskuddFom =
             formatter.format(refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tilskuddFom)
 
         val tilskuddTom =
             formatter.format( refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tilskuddTom)
 
+        if(refusjon.refusjonsgrunnlag.beregning == null){
+            if(refusjon.status != RefusjonStatus.GODKJENT_NULLBELØP) {
+                throw RuntimeException("Beregning er null")
+            }
+            return RefusjonTilPDF(
+                type = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tiltakstype,
+                avtaleNr = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.avtaleNr.toString() + "-" + refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.løpenummer,
+                deltakerFornavn = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.deltakerFornavn,
+                deltakerEtternavn = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.deltakerEtternavn,
+                arbeidsgiverFornavn = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.arbeidsgiverFornavn,
+                arbeidsgiverEtternavn = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.arbeidsgiverEtternavn,
+                arbeidsgiverTlf = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.arbeidsgiverTlf,
+                sendtKravDato = godkjentArbeidsgiverDato,
+                utbetaltKravDato = utbetaltDato,
+                tilskuddFom = tilskuddFom,
+                tilskuddTom = tilskuddTom,
+                kontonummer = refusjon.refusjonsgrunnlag.bedriftKontonummer!!,
+                bedriftKid = bedriftKid,
+                lønn = 0,
+                feriepengerSats = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag!!.feriepengerSats,
+                feriepenger = 0,
+                otpSats = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag!!.otpSats,
+                tjenestepensjon = 0,
+                arbeidsgiveravgiftSats = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.arbeidsgiveravgiftSats,
+                arbeidsgiveravgift = 0,
+                lønnstilskuddsprosent = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.lønnstilskuddsprosent,
+                refusjonsbeløp = 0,
+                beregnetBeløp = 0,
+                overTilskuddsbeløp = false,
+                sumUtgifter = 0,
+                tidligereUtbetalt = 0,
+                fratrekkLønnFerie = 0,
+                lønnFratrukketFerie = 0,
+                tidligereRefundertBeløp = 0,
+                tilskuddsbeløp = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tilskuddsbeløp,
+                forrigeRefusjonMinusBeløp = refusjon.refusjonsgrunnlag.forrigeRefusjonMinusBeløp,
+                forrigeRefusjonsnummer = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.avtaleNr.toString() + "-" + (refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.løpenummer -1),
+                sumUtgifterFratrukketRefundertBeløp = 0
+            )
+        }
+
         return RefusjonTilPDF(
-            refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tiltakstype,
-            refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.avtaleNr.toString() + "-" + refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.løpenummer,
-            refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.deltakerFornavn,
-            refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.deltakerEtternavn,
-            refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.arbeidsgiverFornavn,
-            refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.arbeidsgiverEtternavn,
-            refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.arbeidsgiverTlf,
-            godkjentArbeidsgiverDato,
-            utbetaltDato,
-            tilskuddFom,
-            tilskuddTom,
-            refusjon.refusjonsgrunnlag.bedriftKontonummer!!,
-            bedriftKid,
-            refusjon.refusjonsgrunnlag.beregning!!.lønn,
-            refusjon.refusjonsgrunnlag.tilskuddsgrunnlag!!.feriepengerSats,
-            refusjon.refusjonsgrunnlag.beregning!!.feriepenger,
-            refusjon.refusjonsgrunnlag.tilskuddsgrunnlag!!.otpSats,
-            refusjon.refusjonsgrunnlag.beregning!!.tjenestepensjon,
-            refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.arbeidsgiveravgiftSats,
-            refusjon.refusjonsgrunnlag.beregning!!.arbeidsgiveravgift,
-            refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.lønnstilskuddsprosent,
-            refusjon.refusjonsgrunnlag.beregning!!.refusjonsbeløp,
-            refusjon.refusjonsgrunnlag.beregning!!.beregnetBeløp,
-            refusjon.refusjonsgrunnlag.beregning!!.overTilskuddsbeløp,
-            refusjon.refusjonsgrunnlag.beregning!!.sumUtgifter,
-            refusjon.refusjonsgrunnlag.beregning!!.tidligereUtbetalt,
-            refusjon.refusjonsgrunnlag.beregning!!.fratrekkLønnFerie,
-            refusjon.refusjonsgrunnlag.beregning!!.lønnFratrukketFerie,
-            refusjon.refusjonsgrunnlag.beregning!!.tidligereRefundertBeløp,
-            refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tilskuddsbeløp,
-            refusjon.refusjonsgrunnlag.forrigeRefusjonMinusBeløp,
-            refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.avtaleNr.toString() + "-" + (refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.løpenummer -1),
-            refusjon.refusjonsgrunnlag.beregning!!.sumUtgifterFratrukketRefundertBeløp
+            type = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tiltakstype,
+            avtaleNr = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.avtaleNr.toString() + "-" + refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.løpenummer,
+            deltakerFornavn = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.deltakerFornavn,
+            deltakerEtternavn = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.deltakerEtternavn,
+            arbeidsgiverFornavn = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.arbeidsgiverFornavn,
+            arbeidsgiverEtternavn = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.arbeidsgiverEtternavn,
+            arbeidsgiverTlf = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.arbeidsgiverTlf,
+            sendtKravDato = godkjentArbeidsgiverDato,
+            utbetaltKravDato = utbetaltDato,
+            tilskuddFom = tilskuddFom,
+            tilskuddTom = tilskuddTom,
+            kontonummer = refusjon.refusjonsgrunnlag.bedriftKontonummer!!,
+            bedriftKid = bedriftKid,
+            lønn = refusjon.refusjonsgrunnlag.beregning!!.lønn,
+            feriepengerSats = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag!!.feriepengerSats,
+            feriepenger = refusjon.refusjonsgrunnlag.beregning!!.feriepenger,
+            otpSats = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag!!.otpSats,
+            tjenestepensjon = refusjon.refusjonsgrunnlag.beregning!!.tjenestepensjon,
+            arbeidsgiveravgiftSats = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.arbeidsgiveravgiftSats,
+            arbeidsgiveravgift = refusjon.refusjonsgrunnlag.beregning!!.arbeidsgiveravgift,
+            lønnstilskuddsprosent = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.lønnstilskuddsprosent,
+            refusjonsbeløp = refusjon.refusjonsgrunnlag.beregning!!.refusjonsbeløp,
+            beregnetBeløp = refusjon.refusjonsgrunnlag.beregning!!.beregnetBeløp,
+            overTilskuddsbeløp = refusjon.refusjonsgrunnlag.beregning!!.overTilskuddsbeløp,
+            sumUtgifter = refusjon.refusjonsgrunnlag.beregning!!.sumUtgifter,
+            tidligereUtbetalt = refusjon.refusjonsgrunnlag.beregning!!.tidligereUtbetalt,
+            fratrekkLønnFerie = refusjon.refusjonsgrunnlag.beregning!!.fratrekkLønnFerie,
+            lønnFratrukketFerie = refusjon.refusjonsgrunnlag.beregning!!.lønnFratrukketFerie,
+            tidligereRefundertBeløp = refusjon.refusjonsgrunnlag.beregning!!.tidligereRefundertBeløp,
+            tilskuddsbeløp = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tilskuddsbeløp,
+            forrigeRefusjonMinusBeløp = refusjon.refusjonsgrunnlag.forrigeRefusjonMinusBeløp,
+            forrigeRefusjonsnummer = refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.avtaleNr.toString() + "-" + (refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.løpenummer -1),
+            sumUtgifterFratrukketRefundertBeløp = refusjon.refusjonsgrunnlag.beregning!!.sumUtgifterFratrukketRefundertBeløp
         )
     }
 }

4993aa0fffc0ea050ba90678121ea19aba7a955f
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-03-04 15:42:16 +0100

    Auditlogging - Logg oppslag fra arbeidsgiver på orgnr
    Når en arbeidsgiver slår opp på en deltaker, skal vi logge at et
    oppslag har funnet sted. Men vi bør logge oppslaget på avtalens
    bedriftsnummer ettersom det ikke er ønskelig å logge arbeidsgivers
    fødselsnummer.

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/autorisering/FakeGraphApiService.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/autorisering/FakeGraphApiService.kt
@@ -1,5 +1,7 @@
 package no.nav.arbeidsgiver.tiltakrefusjon.autorisering
 
+import no.nav.arbeidsgiver.tiltakrefusjon.utils.Issuer
+import no.nav.arbeidsgiver.tiltakrefusjon.utils.getClaims
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
@@ -8,7 +10,7 @@ import org.springframework.stereotype.Service
 @ConditionalOnProperty("tiltak-refusjon.graph-api.fake")
 class FakeGraphApiService(val context: TokenValidationContextHolder) : GraphApiService {
     override fun hent(): GraphApiService.GraphApiResponse {
-        val claims = context.getTokenValidationContext().getClaims("aad")
-        return GraphApiService.GraphApiResponse(claims.getStringClaim("NAVident"), "Navn Testnavn")
+        val claims = context.getClaims(Issuer.AZURE)
+        return GraphApiService.GraphApiResponse(claims!!.getStringClaim("NAVident"), "Navn Testnavn")
     }
 }

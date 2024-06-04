1246addf4d999c7cc4531155428955443bf55f36
d142796 <millad.dagdoni@nav.no>
2024-06-04 11:49:08 +0200

    fjernet ekstra bearerTokenInterceptorKontoregister for soko kontoregister

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/SecurityClientConfiguration.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/SecurityClientConfiguration.kt
@@ -1,15 +1,9 @@
 package no.nav.arbeidsgiver.tiltakrefusjon
 
-import no.nav.arbeidsgiver.tiltakrefusjon.okonomi.KontoregisterProperties
 import no.nav.security.token.support.client.core.ClientProperties
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
-import no.nav.security.token.support.client.spring.oauth2.ClientConfigurationPropertiesMatcher
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
-import no.nav.security.token.support.client.spring.oauth2.OAuth2ClientRequestInterceptor
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -18,20 +12,15 @@ import org.springframework.http.HttpRequest
 import org.springframework.http.client.ClientHttpRequestExecution
 import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.web.client.RestTemplate
-import java.net.URI
-import java.util.*
 
 @EnableOAuth2Client(cacheEnabled = true)
 @Configuration
 @Profile("dev-gcp", "prod-gcp")
 class SecurityClientConfiguration(
-    val properties: KontoregisterProperties,
-    val restTemplateBuilder: RestTemplateBuilder,
-    val clientConfigurationProperties: ClientConfigurationProperties,
-    val oAuth2AccessTokenService: OAuth2AccessTokenService
+        val restTemplateBuilder: RestTemplateBuilder,
+        val clientConfigurationProperties: ClientConfigurationProperties,
+        val oAuth2AccessTokenService: OAuth2AccessTokenService
 ) {
-
-    val log: Logger = LoggerFactory.getLogger(javaClass)
 
     @Bean
     fun påVegneAvSaksbehandlerGraphRestTemplate() = restTemplateForRegistration("aad-graph")
@@ -52,33 +41,10 @@ class SecurityClientConfiguration(
     private fun restTemplateForRegistration(registration: String): RestTemplate {
         val clientProperties = clientConfigurationProperties.registration[registration]
                 ?: throw RuntimeException("could not find oauth2 client config for $registration")
-        when(registration) {
-            "sokos-kontoregister" -> {
-                val restTemplate: RestTemplate = restTemplateBuilder
-                    .additionalInterceptors(
-                        bearerTokenInterceptorKontoregister(
-                            clientProperties,
-                            oAuth2AccessTokenService
-                        )
-                    )
-                    .build()
-                return restTemplate
-            }
-            else -> {
-                val restTemplate: RestTemplate = restTemplateBuilder
-                    .additionalInterceptors(
-                        bearerTokenInterceptor(
-                            clientProperties,
-                            oAuth2AccessTokenService
-                        )
-                    )
-                    .build()
-                return restTemplate
-            }
-
-        }
+        return restTemplateBuilder
+                .additionalInterceptors(bearerTokenInterceptor(clientProperties, oAuth2AccessTokenService))
+                .build()
     }
-
 
     private fun bearerTokenInterceptor(
             clientProperties: ClientProperties,
@@ -87,21 +53,6 @@ class SecurityClientConfiguration(
         return ClientHttpRequestInterceptor { request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution ->
             val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
             request.headers.setBearerAuth(response.accessToken!!)
-            execution.execute(request, body)
-        }
-    }
-
-    private fun bearerTokenInterceptorKontoregister(
-            clientProperties: ClientProperties,
-            oAuth2AccessTokenService: OAuth2AccessTokenService
-    ): ClientHttpRequestInterceptor {
-        return ClientHttpRequestInterceptor { request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution ->
-            log.warn("#### OAUTH CALL: ${clientProperties.tokenEndpointUrl} ### ${clientProperties.scope} ### ${clientProperties.grantType}");
-            val response = oAuth2AccessTokenService.getAccessToken(clientProperties) //todo feil her!!!!
-            request.headers.setBearerAuth(response.accessToken!!)
-            request.headers.set("Nav-Consumer-Id",properties.consumerId)
-            request.headers.set("Nav-Call-Id",UUID.randomUUID().toString())
-            log.warn("#### HEADERS: ${request.headers}");
             execution.execute(request, body)
         }
     }

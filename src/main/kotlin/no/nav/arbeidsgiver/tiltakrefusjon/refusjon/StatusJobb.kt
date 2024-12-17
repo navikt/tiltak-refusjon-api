cfba2d14231a65f0576c206d4662d2d1d3ea6f55
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-12-17 15:38:57 +0100

    Flytt EnableXYZ-annotasjoner til applikasjonsklasse

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/StatusJobb.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/StatusJobb.kt
@@ -3,12 +3,10 @@ package no.nav.arbeidsgiver.tiltakrefusjon.refusjon
 import no.nav.arbeidsgiver.tiltakrefusjon.leader.LeaderPodCheck
 import no.nav.arbeidsgiver.tiltakrefusjon.utils.Now
 import org.slf4j.LoggerFactory
-import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
-@EnableScheduling
 class StatusJobb(
     val refusjonRepository: RefusjonRepository,
     val leaderPodCheck: LeaderPodCheck,

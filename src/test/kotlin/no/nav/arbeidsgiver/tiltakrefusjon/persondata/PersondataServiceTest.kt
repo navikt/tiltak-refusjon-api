88fa13c166a4fff3c09726b1d3509f54202d0fe9
Eirik Vigeland <eirik.vigeland@nav.no>
2025-04-11 14:52:50 +0200

    fix. oppdatert wiremock oppsett med litt flere cases fra pdl

--- src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/persondata/PersondataServiceTest.kt
+++ src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/persondata/PersondataServiceTest.kt
@@ -5,10 +5,8 @@ import no.nav.team_tiltak.felles.persondata.pdl.domene.Diskresjonskode
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
-import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
@@ -18,7 +16,6 @@ class PersondataServiceTest {
     final val STRENGT_FORTROLIG_UTLAND_FNR = Fnr("28033114267")
     final val STRENGT_FORTROLIG_FNR = Fnr("16053900422")
     final val FORTROLIG_FNR = Fnr("26067114433")
-    final val UGRADERT_FNR = Fnr("08120689976")
     final val UGRADERT_PERSON_TOM_RESPONSE_FNR = Fnr("23097010706")
 
     @Autowired
@@ -30,17 +27,15 @@ class PersondataServiceTest {
             STRENGT_FORTROLIG_UTLAND_FNR,
             STRENGT_FORTROLIG_FNR,
             FORTROLIG_FNR,
-            UGRADERT_FNR,
             UGRADERT_PERSON_TOM_RESPONSE_FNR
         )
 
         val diskresjonskoder = persondataService.hentDiskresjonskoder(fnrSet);
-        assertThat(diskresjonskoder).hasSize(5)
+        assertThat(diskresjonskoder).hasSize(4)
 
         assertThat(diskresjonskoder[STRENGT_FORTROLIG_UTLAND_FNR]).isEqualTo(Diskresjonskode.STRENGT_FORTROLIG_UTLAND)
         assertThat(diskresjonskoder[STRENGT_FORTROLIG_FNR]).isEqualTo(Diskresjonskode.STRENGT_FORTROLIG)
         assertThat(diskresjonskoder[FORTROLIG_FNR]).isEqualTo(Diskresjonskode.FORTROLIG)
-        assertThat(diskresjonskoder[UGRADERT_FNR]).isEqualTo(Diskresjonskode.UGRADERT)
         assertThat(diskresjonskoder[UGRADERT_PERSON_TOM_RESPONSE_FNR]).isEqualTo(Diskresjonskode.UGRADERT)
     }
 

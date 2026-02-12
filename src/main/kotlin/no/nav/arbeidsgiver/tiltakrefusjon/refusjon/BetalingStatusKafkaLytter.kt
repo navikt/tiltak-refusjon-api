991d190a0ff8287b7cc1ef20d396980d1b9615a4
d142796 <millad.dagdoni@nav.no>
2026-02-12 13:20:46 +0100

    logger om vi i det hele tatt mottar betalingsstatus endringer fra økonomi.

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/BetalingStatusKafkaLytter.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/BetalingStatusKafkaLytter.kt
@@ -52,6 +52,8 @@ class BetalingStatusKafkaLytter(
     @KafkaListener(topics = [REFUSJON_ENDRET_BETALINGSSTATUS])
     fun oppdaterKorreksjonEllerRefusjonStatusBasertPåBetalingStatusFraØkonomi(event: String) {
         val betalingsstatus = objectMapper.readValue(event, BetalingStatusEndringMelding::class.java)
+        log.info("Mottatt en betalingsstatus som skal behandles for avtale ${betalingsstatus.avtaleNr} med betalingsstatus ${betalingsstatus.status}")
+
         if (betalingsstatus.erForRefusjon() && betalingsstatus.erForKorreksjon()) {
             log.error(
                 "Betalingsstatus for både korreksjon og refusjon mottatt! Id: {}, refusjonId: {}, korreksjonId: {}",

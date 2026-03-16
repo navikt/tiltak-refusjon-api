b221d64e80998f60ae2e43c9ef2912f409d3664f
Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>
2026-03-16 09:46:19 +0100

    Revertere fjerning av mockserver

---
+++ src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/IntegrasjonerMockServer.kt
@@ -0,0 +1,29 @@
+package no.nav.arbeidsgiver.tiltakrefusjon
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.common.ConsoleNotifier
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import org.springframework.beans.factory.DisposableBean
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
+
+@Profile("wiremock")
+@Component
+class IntegrasjonerMockServer(@Value("\${wiremock.port}") val wiremockPort: Int) : DisposableBean {
+    private val server: WireMockServer = WireMockServer(WireMockConfiguration
+        .options()
+        .usingFilesUnderClasspath(".")
+        .notifier(ConsoleNotifier(false))
+        .port(wiremockPort))
+
+    init {
+        server.start()
+    }
+
+    fun getServer() = server
+
+    override fun destroy() {
+        server.stop()
+    }
+}

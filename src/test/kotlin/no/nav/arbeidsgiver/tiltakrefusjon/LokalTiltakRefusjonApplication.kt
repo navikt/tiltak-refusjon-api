6237b836901fc8e236bcf7517c31b7d8f885a0e0
Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>
2026-03-27 10:51:24 +0100

    Fjern ubrukt import

--- src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/LokalTiltakRefusjonApplication.kt
+++ src/test/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/LokalTiltakRefusjonApplication.kt
@@ -1,6 +1,5 @@
 package no.nav.arbeidsgiver.tiltakrefusjon
 
-import no.bekk.bekkopen.person.FodselsnummerValidator
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Profile

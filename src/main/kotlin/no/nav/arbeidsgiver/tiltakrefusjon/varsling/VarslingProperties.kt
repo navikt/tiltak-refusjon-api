f349599d9483584e42787b106a18d615feba512f
Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>
2026-04-09 10:37:32 +0200

    Fjern ubrukt konfigurasjonsklasse

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/varsling/VarslingProperties.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.arbeidsgiver.tiltakrefusjon.varsling
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.stereotype.Component
-
-@Component
-@ConfigurationProperties(prefix = "tiltak-refusjon.varslingsjobb")
-data class VarslingProperties(val earlyBirds: List<String>)

7bb1acec569073f0c927167ad85ede5e52311a17
Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>
2026-05-05 15:38:41 +0200

    Fjern rester etter tiltak-proxy
    Vi bruker ikke lenger tiltak-proxy, og behøver derfor
    heller ikke å definere en kobling til proxy sin host-url.
    
    Vi har heller ingen api-koblinger til microsoft sine api,
    dette ble gjort i en tid hvor vi trodde vi måtte bruke
    graph-apiet for å hente innlogget bruker sitt navn.
    
    En morsom detalj er at når vi fjerner "api-scope" for
    tiltak-proxy så må vi fjerne en rest template som ble
    brukt i Norg2-kallene våre. Disse kallene utførte vi
    sannsynligvis via tiltak-proxy tidligere, og etter vi
    begynte å kalle Norg2 direkte så har de ignorert JWT-tokenet
    vi legger ved, som er for tiltak-proxy!
    
    Det betyr at dersom vi hadde lagt ned tiltak-proxy først,
    så ville denne appen begynt å feile fordi den ikke får
    opprettet ubrukelige tokens.

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/norg/NorgServiceImp.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/norg/NorgServiceImp.kt
@@ -2,22 +2,24 @@ package no.nav.arbeidsgiver.tiltakrefusjon.norg
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForEntity
 
 @Service
 @Profile("dev-gcp", "prod-gcp")
-class NorgServiceImp(@Qualifier("anonymProxyRestTemplate") val restTemplate: RestTemplate, val properties: NorgProperties) : NorgService {
+class NorgServiceImp(
+    private val noAuthRestTemplate: RestTemplate,
+    private val properties: NorgProperties
+) : NorgService {
     val log: Logger = LoggerFactory.getLogger(javaClass)
 
     override fun hentEnhetNavn(enhet: String): String? {
         val uri = properties.uri + "/enhet/{enhet}"
         try {
-            val norgResponse = restTemplate.getForEntity(
+            val norgResponse = noAuthRestTemplate.getForEntity<NorgEnhetResponse>(
                 uri,
-                NorgEnhetResponse::class.java,
                 mapOf("enhet" to enhet)
             )
             return norgResponse.body?.navn

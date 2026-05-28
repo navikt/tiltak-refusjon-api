ce01ed79a1d42ad79d33732a4a0a3a9bdcc6ebbe
Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>
2026-05-28 11:23:05 +0200

    Sum utbetalt må gjelde fireårig lønnstilskudd
    Vi har en bug hvor sum utbetalt ikke beregnes for fireårig
    lønnstilskudd, fordi vi aldri teller opp en totalsum for
    dette tiltaket.
    
    I tillegg har aldri korreksjoner blitt inkludert i totalsum,
    og dermed kan det potensielt være mulig å korrigere seg over
    5g-grensen for en avtale.
    
    OBS!!! Denne committen er ikke en endelig fiks for problemet
    med korreksjoner, fordi vi kun oppdaterer summen på åpne
    refusjoner og korreksjoner dersom en refusjon oppdateres.
    Har avgrenset denne committen for å minimere antall endringer.

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/KorreksjonRepository.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/KorreksjonRepository.kt
@@ -3,4 +3,11 @@ package no.nav.arbeidsgiver.tiltakrefusjon.refusjon
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface KorreksjonRepository : JpaRepository<Korreksjon, String> {
+
+    fun findAllByDeltakerFnrAndBedriftNrAndStatusInAndRefusjonsgrunnlag_Tilskuddsgrunnlag_Tiltakstype(
+        deltakerFnr: String,
+        bedriftNr: String,
+        status: List<Korreksjonstype>,
+        tiltakstype: Tiltakstype
+    ): List<Korreksjon>
 }

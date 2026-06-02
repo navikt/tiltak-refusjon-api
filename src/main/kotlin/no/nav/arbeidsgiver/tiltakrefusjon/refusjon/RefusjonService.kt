9a2c6f35258125f4e97c40cae50901eed2918c7a
Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>
2026-06-02 15:10:13 +0200

    Fiks kommentar

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonService.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/refusjon/RefusjonService.kt
@@ -341,7 +341,7 @@ class RefusjonService(
 
     fun oppdaterRefusjon(refusjon: Refusjon, utførtAv: InnloggetBruker) {
         log.info("Oppdaterer refusjon ${refusjon.id} med data")
-        // Ikke oppdater tallgrunnlag på innsendte refusjoner
+        // Skal kun mutere refusjonsgrunnalget for refusjoner som skal sendes inn
         if (refusjon.status.isUbehandlet()) {
             settMinusbeløpFraTidligereRefusjonerTilknyttetAvtalen(refusjon)
             if (refusjon.tiltakstype().har5gBegrensning()) {

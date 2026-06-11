d89dc02144be5846df44537f0abf6c485a9d36ce
Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>
2026-06-11 13:48:33 +0200

    Forsøke på en backfill av opprettet_tidspunkt

--- src/main/resources/db/migration/V63__refusjon_opprettet_tidspunkt.sql
+++ src/main/resources/db/migration/V63__refusjon_opprettet_tidspunkt.sql
@@ -1,1 +1,7 @@
 alter table refusjon add column opprettet_tidspunkt TIMESTAMP WITH TIME ZONE;
+
+update refusjon
+set opprettet_tidspunkt =
+        (select hendelseslogg.tidspunkt at time zone 'UTC'
+         from hendelseslogg
+         where hendelseslogg.refusjon_id = refusjon.id and hendelseslogg.event = 'RefusjonOpprettet');

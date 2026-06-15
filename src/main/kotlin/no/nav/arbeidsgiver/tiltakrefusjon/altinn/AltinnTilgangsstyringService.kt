68d7ce2a421096e8339bbc7bda9062a0f0bf5e95
Odd Andreas Sørsæther <3494925+Oddsor@users.noreply.github.com>
2026-06-15 10:15:04 +0200

    Adressesperre-sjekk bruker samme respons som tiltaksrefusjon
    Når vi ber om altinn3tilganger for tiltaksrefusjon så vil
    responsen også inneholde informasjon om hvorvidt arbeidsgiveren har
    tilgang til adressesperre på samme bedrift.

--- src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/altinn/AltinnTilgangsstyringService.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/tiltakrefusjon/altinn/AltinnTilgangsstyringService.kt
@@ -1,18 +1,13 @@
 package no.nav.arbeidsgiver.tiltakrefusjon.altinn
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import no.nav.arbeidsgiver.tiltakrefusjon.Feilkode
 import no.nav.arbeidsgiver.tiltakrefusjon.FeilkodeException
 import no.nav.arbeidsgiver.tiltakrefusjon.utils.flatUtHierarki
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.postForObject
-import org.springframework.web.util.UriComponentsBuilder
 
 @Service
 class AltinnTilgangsstyringService(
@@ -20,44 +15,28 @@ class AltinnTilgangsstyringService(
     @Qualifier("påVegneAvArbeidsgiverAltinn3RestTemplate")
     val restTemplateAltinn3: RestTemplate
 ) {
-    private val objectMapper: ObjectMapper = ObjectMapper()
     private val logger = LoggerFactory.getLogger(javaClass)
 
     fun hentInntektsmeldingEllerRefusjonTilganger(): Set<Organisasjon> {
+        return heltAltinnTilganger().tilGammeltFormat().apply {
+            logger.debug("Refusjon-tilganger for arbeidsgiver: {}", this.size)
+        }
+    }
+
+    fun heltAltinnTilganger(): List<AltinnTilgang> {
         val altinnTilgangerRequest = AltinnTilgangerRequest(
             filter = Filter(
                 altinn3Tilganger = setOf("nav_tiltak_tiltaksrefusjon"),
                 inkluderSlettede = false
             )
         )
-        return heltAltinnTilganger(altinnTilgangerRequest)
-    }
-
-    fun heltAltinnTilganger(altinnTilgangerRequest: AltinnTilgangerRequest): Set<Organisasjon> {
         val response = kallAltinn3(altinnTilgangerRequest)
         logger.debug("Respons fra altinn: {}", response)
         val løvnoderOgParents = flatUtHierarki(response)
-        val organisasjonerPåGammeltFormat = løvnoderOgParents.flatMap { org ->
-            listOf(Organisasjon(
-                organizationNumber = org.orgnr,
-                name = org.navn,
-                organizationForm = org.organisasjonsform,
-                type = "Enterprise",
-                status = if (org.erSlettet) "Inactive" else "Active",
-                parentOrganizationNumber = null
-            )) + org.underenheter.map { underenhet ->
-                Organisasjon(
-                    organizationNumber = underenhet.orgnr,
-                    name = underenhet.navn,
-                    organizationForm = underenhet.organisasjonsform,
-                    type = "Business",
-                    status = if (underenhet.erSlettet) "Inactive" else "Active",
-                    parentOrganizationNumber = org.orgnr
-                )
-            }
+        løvnoderOgParents.filter { it.altinn3Tilganger.contains("nav_tiltak_adressesperre") }
+        return løvnoderOgParents.apply {
+            logger.debug("Altinn-tilganger for arbeidsgiver: {}", this.size)
         }
-        logger.debug("Altinn-tilganger for arbeidsgiver: {}", organisasjonerPåGammeltFormat)
-        return organisasjonerPåGammeltFormat.toSet()
     }
 
     fun kallAltinn3(altinnTilgangerRequest: AltinnTilgangerRequest): List<AltinnTilgang> {
@@ -74,13 +53,35 @@ class AltinnTilgangsstyringService(
     }
 
     fun hentAdressesperreTilganger(): Set<Organisasjon> {
-        val altinnTilgangerRequest = AltinnTilgangerRequest(
-            filter = Filter(
-                altinn3Tilganger = setOf("nav_tiltak_adressesperre"),
-                inkluderSlettede = false
-            )
-        )
-        return heltAltinnTilganger(altinnTilgangerRequest)
+        return heltAltinnTilganger()
+            .filter { it.altinn3Tilganger.contains("nav_tiltak_adressesperre") }
+            .tilGammeltFormat()
+            .apply {
+                logger.debug("Adressesperre-tilganger for arbeidsgiver: {}", this.size)
+            }
     }
 
 }
+
+private fun List<AltinnTilgang>.tilGammeltFormat(): Set<Organisasjon> =
+    this.flatMap { org ->
+        listOf(
+            Organisasjon(
+                organizationNumber = org.orgnr,
+                name = org.navn,
+                organizationForm = org.organisasjonsform,
+                type = "Enterprise",
+                status = if (org.erSlettet) "Inactive" else "Active",
+                parentOrganizationNumber = null
+            )
+        ) + org.underenheter.map { underenhet ->
+            Organisasjon(
+                organizationNumber = underenhet.orgnr,
+                name = underenhet.navn,
+                organizationForm = underenhet.organisasjonsform,
+                type = "Business",
+                status = if (underenhet.erSlettet) "Inactive" else "Active",
+                parentOrganizationNumber = org.orgnr
+            )
+        }
+    }.toSet()
